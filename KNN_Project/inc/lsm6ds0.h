846bac9612dec0e9a1a38b747056eeb794458bc3
andre <andreparo63@hotmail.it>
2018-02-21 23:50:38 +0100

    codice jacopo per accelerometro e giroscopio

--- KNN_Project/inc/lsm6ds0.h
+++ KNN_Project/inc/lsm6ds0.h
@@ -4,7 +4,7 @@
  *  Created on: 21 feb 2018
  *      Author: andre
  */
-
+/*
 #ifndef LSM6DS0_H_
 #define LSM6DS0_H_
 
@@ -37,3 +37,46 @@ void	LSM6DS0_ReadCalib(void);
 
 
 #endif /* LSM6DS0_H_ */
+
+ #ifndef LSM6DS0_H_
+ #define LSM6DS0_H_
+
+ #include "i2c.h"
+
+ #define LSM6DS0_REFERENCE_G  	0x0B
+ #define LSM6DS0_WHO_AM_I  		0x0F
+ #define LSM6DS0_CTRL_REG1_G  	0X10 //Modificare questi tre basandosi su tabella pag.39
+ #define LSM6DS0_CTRL_REG2_G	0x11
+ #define LSM6DS0_CTRL_REG3_G	0x12
+ #define LSM6DS0_ORIENT_CFG_G	0X13
+ #define LSM6DS0_OUT_X_L_G		0x18
+ #define LSM6DS0_OUT_X_H_G		0x19
+ #define LSM6DS0_OUT_Y_L_G		0x1A
+ #define LSM6DS0_OUT_Y_H_G		0x1B
+ #define LSM6DS0_OUT_Z_L_G		0x1C
+ #define LSM6DS0_OUT_Z_H_G 		0x1D
+ #define LSM6DS0_CTRL_REG4		0X1E
+ #define LSM6DS0_CTRL_REG5_XL	0X1F
+ #define LSM6DS0_CTRL_REG6_XL	0X20
+ #define LSM6DS0_CTRL_REG7_XL	0X21
+ #define LSM6DS0_CTRL_REG8		0X22
+ #define LSM6DS0_CTRL_REG9		0X23
+ #define LSM6DS0_CTRL_REG10		0X24
+ #define LSM6DS0_OUT_X_L_XL		0X28
+ #define LSM6DS0_OUT_X_H_XL		0X29
+ #define LSM6DS0_OUT_Y_L_XL		0X2A
+ #define LSM6DS0_OUT_Y_H_XL		0X2B
+ #define LSM6DS0_OUT_Z_L_XL		0X2C
+ #define LSM6DS0_OUT_Z_H_XL		0X2D
+
+ FunctionalState LSM6DS0_present;
+
+ //Definizione variabili di uscita/di conversione/ecc.. ->
+ //
+
+ void LSM6DS0_Config(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t); //Chiedere Ansovini per registri
+ uint8_t LSM6DS0_WhoAmI(void);
+ float[3] LSM6DS0_ReadAcceleration(void);
+ float[3] LSM6DS0_ReadDirection(void);
+
+ #endif
--- KNN_Project/src/lsm6ds0.c
+++ KNN_Project/src/lsm6ds0.c
@@ -4,6 +4,94 @@
  *  Created on: 21 feb 2018
  *      Author: andre
  */
+
+#include "LSM6DS0.h"
+
+ uint8_t LSM6DS0_WhoAmI(void)
+{
+	const uint8_t I2C_RXBUFFERSIZE = 1;
+	uint8_t I2C_RxBuffer[I2C_RXBUFFERSIZE];
+
+	I2C_RxBuffer][0] = LSM6DS0_Who_Am_I;
+
+	HAL_I2C_Master_Transmit(&I2C1Handle, (uint16_t)LSM6DS0_add<<1  & 0xFE, (uint8_t *)&I2C_RxBuffer[0], 1, 10000);
+	HAL_I2C_Master_Receive(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 | 1, (uint8_t *)&I2C_RxBuffer[0], 1, 10000);
+
+	return I2C_RxBuffer[0];
+}
+
+
+ void LSM6DS0_Config(uint8_t ctrl1g, uint8_t ctrl2g, uint8_t ctrl3g, uint8_t ctrl4, uint8_t ctrl5xl, uint8_t ctrl6xl, uint8_t ctrl7xl, uint8_t ctrl8, uint8_t ctrl9, uint8_t ctrl10)
+{
+ const uint8_t I2C_TXBUFFERSIZE = 10;
+
+ uint8_t I2C_TxBuffer[I2C_TXBUFFERSIZE];
+
+ I2C_TxBuffer[0] = ctrl1g;
+ I2C_TxBuffer[1] = ctrl2g;
+ I2C_TxBuffer[2] = ctrl3g;
+ I2C_TxBuffer[3] = ctrl4;
+ I2C_TxBuffer[4] = ctrl5xl;
+ I2C_TxBuffer[5] = ctrl6xl;
+ I2C_TxBuffer[6] = ctrl7xl;
+ I2C_TxBuffer[7] = ctrl8;
+ I2C_TxBuffer[8] = ctrl9;
+ I2C_TxBuffer[9] = ctrl10;
+
+
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG1_G, 1, (uint8_t *)&I2C_TxBuffer[0], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG2_G, 1, (uint8_t *)&I2C_TxBuffer[1], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG3_G, 1, (uint8_t *)&I2C_TxBuffer[2], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG4, 1, (uint8_t *)&I2C_TxBuffer[3], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG5_XL, 1, (uint8_t *)&I2C_TxBuffer[4], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG6_XL, 1, (uint8_t *)&I2C_TxBuffer[5], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG7_XL, 1, (uint8_t *)&I2C_TxBuffer[6], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG8, 1, (uint8_t *)&I2C_TxBuffer[7], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG9, 1, (uint8_t *)&I2C_TxBuffer[8], 1, 10000);
+ HAL_I2C_Mem_Write(&I2C1Handle, (uint16_t)LSM6DS0_add<<1 & 0xFE, LSM6DS0_CTRL_REG10, 1, (uint8_t *)&I2C_TxBuffer[9], 1, 10000);
+}
+
+//Da inserire la lettura dei dati (->Vedi ultimo lab, commento di seguito)
+float[3] LSM6DS0_ReadAcceleration(void)
+{
+	return acceleration;
+}
+float[3] LSM6DS0_ReadDirection(void)
+{
+	return direction;
+}
+
+/*float LPS25HB_ReadPressure(void)
+{
+ const uint8_t I2C_RXBUFFERSIZE = 3;
+
+ uint8_t I2C_RxBuffer[I2C_RXBUFFERSIZE];
+
+ HAL_I2C_Mem_Read(&I2C1Handle, (uint16_t)LPS25HB_add<<1 | 1, LPS25HB_press_XL, 1, (uint8_t *)&I2C_RxBuffer[0], 1, 10000);
+ HAL_I2C_Mem_Read(&I2C1Handle, (uint16_t)LPS25HB_add<<1 | 1, LPS25HB_press_L, 1, (uint8_t *)&I2C_RxBuffer[1], 1, 10000);
+ HAL_I2C_Mem_Read(&I2C1Handle, (uint16_t)LPS25HB_add<<1 | 1, LPS25HB_press_H, 1, (uint8_t *)&I2C_RxBuffer[2], 1, 10000);
+ int32_t data = ((uint32_t)I2C_RxBuffer[2]<<16 | (uint16_t)I2C_RxBuffer[1]<<8 | I2C_RxBuffer[0]);
+ return (float)data/4096;
+}
+
+
+float HTS221_ReadTemperature(void)
+{
+ const uint8_t I2C_RXBUFFERSIZE = 2;
+
+ uint8_t I2C_RxBuffer[I2C_RXBUFFERSIZE];
+ float Temperature;
+
+ HAL_I2C_Mem_Read(&I2C1Handle, (uint16_t)HTS221_add<<1 | 1, HTS221_TempL, 1, (uint8_t *)&I2C_RxBuffer[0], 1, 10000);
+ HAL_I2C_Mem_Read(&I2C1Handle, (uint16_t)HTS221_add<<1 | 1, HTS221_TempH, 1, (uint8_t *)&I2C_RxBuffer[1], 1, 10000);
+ int16_t data = (((uint16_t)I2C_RxBuffer[1]<<8 | I2C_RxBuffer[0]));
+ Temperature = (float)data*mT+(float)HTS221_T0_degC_x8/8;
+ return Temperature;
+}*/
+
+
+
+/*
 #include "lsm6ds0.h"
 
 
@@ -100,4 +188,4 @@ void LSM6DS0_ReadCalib(void)
 	mH=(((float)LSM6DS0_H1_rHx2/2)-((float)LSM6DS0_H0_rHx2/2))/(LSM6DS0_H1_T0_Out-LSM6DS0_H0_T0_Out);
 
 }
-
+*/
